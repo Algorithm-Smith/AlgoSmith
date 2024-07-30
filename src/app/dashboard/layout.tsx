893c2ed9af0f4e271374002d4fa457d2b5cee50d
Lauren <laurenfelty01@gmail.com>
2024-07-30 11:56:28 -0500

    update tailwind css on dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,20 +1,13 @@
 import Sidebar from './components/Sidebar';
 
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
       <div className="w-1/4">
         <Sidebar />
       </div>
 
-      <div className="flex-1 p-4">
-        {children}
-      </div>
+      <div className="flex-1 p-4">{children}</div>
     </div>
-  )
-};
-
+  );
+}
