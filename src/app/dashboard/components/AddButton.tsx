c0a6c40102e578042152b917d5626f8eb9e08508
Lauren <laurenfelty01@gmail.com>
2024-07-30 11:56:28 -0500

    update tailwind css on dashboard

--- src/app/dashboard/components/AddButton.tsx
+++ src/app/dashboard/components/AddButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AddButton = () => {
   return (
-    <div>
+    <div className='flex flex-col justify-center m-5 text-center'>
       <button
         type='button'
         className='py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
