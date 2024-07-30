63d6e2741af8600110f00c76f4a49d06ee4a82c9
Lauren <laurenfelty01@gmail.com>
2024-07-30 17:15:12 -0500

    update landing page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,34 +8,27 @@ export default function Navbar() {
   const isDevelopment = process.env.NODE_ENV === 'development';
 
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link
-          href="/"
-          className="text-orange-500 text-xl font-bold"
-        >
-          Next.Algo / AL.Go / AlgoSmith
+    <nav className='bg-gray-800 p-4'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link href='/' className='text-orange-500 text-xl font-bold'>
+          AlgoSmith
         </Link>
         <div>
-          <Link
-            href="/"
-            className="text-white mr-4"
-          >
+          <Link href='/' className='text-white mr-4'>
             Home
           </Link>
           {user && (
-            <Link
-              href="/dashboard"
-              className="text-white mr-4"
-            >
+            <Link href='/dashboard' className='text-white mr-4'>
               Dashboard
             </Link>
           )}
           {isDevelopment && (
             <Link
-              href="/dev-debug-dashboard"
+              href='/dev-debug-dashboard'
               className={`font-bold mr-4 ${
-                user ? 'text-green-400 hover:text-green-300' : 'text-red-400 hover:text-red-300'
+                user
+                  ? 'text-green-400 hover:text-green-300'
+                  : 'text-red-400 hover:text-red-300'
               }`}
             >
               Dev Debug
@@ -44,17 +37,11 @@ export default function Navbar() {
           {!isLoading && (
             <>
               {user ? (
-                <a
-                  href="/api/auth/logout"
-                  className="text-white"
-                >
+                <a href='/api/auth/logout' className='text-white'>
                   Logout
                 </a>
               ) : (
-                <a
-                  href="/api/auth/login"
-                  className="text-white"
-                >
+                <a href='/api/auth/login' className='text-white'>
                   Login
                 </a>
               )}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,11 @@
+import logo from '../../public/AlgoSmith.png';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <h1>Wecome to AlgoSmith</h1>
-
-      <a
-        href="/api/auth/login"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-      >
-        <h2 className="mb-3 text-2xl font-semibold">
-          Login(old){' '}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">IN</p>
-      </a>
-      <a
-        href="/api/auth/logout"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-      >
-        <h2 className="mb-3 text-2xl font-semibold">
-          Logout{' '}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">OUT</p>
-      </a>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">Explore starter templates for Next.js.</p>
-        </a>
-      </div> */}
-    </main>
+    <div className='flex flex-col items-center justify-start min-h-screen mb-4 text-4xl font-extrabold leading-none tracking-tight  md:text-5xl lg:text-6xl '>
+      <h1 className='text-gray-800 m-10'>Welcome to AlgoSmith</h1>
+      <Image className='h-auto max-w-lg rounded-lg' src={logo} alt='logo' />
+    </div>
   );
 }
